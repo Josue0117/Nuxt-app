d1b08efa4406460093eaee54d9090fd00a2a4a04
Josue0117 <josue.h.r@outlook.com>
2021-04-09 15:04:57 -0500

    New Folder assets/svg/

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,17 +1,21 @@
 <template>
-    <div>
-
+    <div class="Hero container">
+        <div class="text-container">
+            <h1 class="header">Find your Fire Extinguisher</h1>
+            <p class="snippet">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste iusto illum quidem asperiores, cum nisi aperiam eius ipsa sapiente odit!</p>
+            <button class="button btn">Start Looking</button>
+        </div>
+        <img class="image" src="~assets/svg/fire-extinguisher.svg" alt="fire extinguisher">
     </div>
 </template>
 
 <script>
     export default {
-        
     }
 </script>
 
 <style scoped>
-        .Hero {
+    .Hero {
         height: 50vh;
         position: relative;
         display: flex;
