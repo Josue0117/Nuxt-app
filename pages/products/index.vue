080019d3af59a764c3bc0de18d813c1c07c8fbc7
Josue0117 <josue.h.r@outlook.com>
2021-04-11 23:06:56 -0500

    New folder within pages: products

---
+++ pages/products/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <Nav />
+        <div class="container">
+            <large-card-display
+            v-for="largeInfo in largeCardInfo"
+            :key="largeInfo.id"
+            :tittle="largeInfo.title"
+            :snippet="largeInfo.snippet"
+            :cards="largeInfo.cards"
+        />
+        <small-card-display
+            v-for="smallInfo in smallCardInfo"
+            :key="smallInfo.id"
+            :tittle="smallInfo.title"
+            :cards="smallInfo.cards"
+        />
+        </div>
+    </div>
+</template>
+
+<script>
+import { largeCardSections , smallCardSections } from '@/assets/data.js'
+import LargeCardDisplay from '../../components/LargeCardDisplay.vue'
+import SmallCardDisplay from '../../components/SmallCardDisplay.vue'
+export default {
+    components: { LargeCardDisplay, SmallCardDisplay },
+    data() {
+        return {
+            largeCardInfo: largeCardSections,
+            smallCardInfo: smallCardSections,
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
